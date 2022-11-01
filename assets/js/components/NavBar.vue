65094d9b72503e3e0f399b8ffa36a009468e9dcf
reinoutkoninkx <reinout89koninkx@gmail.com>
2022-11-01 16:32:35 +0100

    navbar

--- assets/js/components/NavBar.vue
+++ assets/js/components/NavBar.vue
@@ -2,8 +2,15 @@
   <nav>
     <n-card id="container-nav">
         <img src="../../images/faviconpic.png"/>
-        <span>Developer Platform</span>
-        <DarkModeSwitchVue />
+        <div id="nav-right">
+            <span>
+                Developer
+                <br />
+                Platform
+            </span>
+            <DarkModeSwitchVue />
+        </div>
+        
 
     </n-card>
     
@@ -23,28 +30,39 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap');
-    nav {
+    
+
+nav {
         display: flex;
         border-bottom: 1px solid black;
         justify-content: space-between;
         font-family: Titillium Web, sans-serif;
-        
+        box-sizing: border-box;
+    }
+
+
+    nav #nav-right {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 85%;
+        margin: 0;
     }
 
     nav #container-nav {
         display: flex;
+        padding: 0;
     }
 
     nav img {
-        width: 30px;
-        height: 30px;
-        margin: 10px;
+        width: 15%;
+        margin: 5px;
     }
 
     nav span {
-        font-size: 20px;
+        font-size: 15px;
         margin: 10px;
 
     }
